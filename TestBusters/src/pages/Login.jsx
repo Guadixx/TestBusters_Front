1e4a5e0bc5cda464caaa1f8db7c5d4659286b573
Guadixx <guadadoud@gmail.com>
2023-03-23 10:36:16 +0100

    feat.Login

--- TestBusters/src/pages/Login.jsx
+++ TestBusters/src/pages/Login.jsx
@@ -3,11 +3,13 @@ import './Login.css';
 import { useContext, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import { UserContext } from '../context/UserContext';
 import { API } from '../services/API';
 
 const Login = () => {
+  const [onFocus, setOnFocus] = useState(false);
   const [see, setSee] = useState(false);
   const handleClick = (ev) => {
     ev.preventDefault();
@@ -33,30 +35,71 @@ const Login = () => {
       });
   };
   return (
-    <main>
-      <h2>Login</h2>
-      {error !== null && <h2>{error}</h2>}
-      <form onSubmit={handleSubmit(formSubmit)}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          placeholder="username"
-          id="username"
-          name="username"
-          {...register('username')}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type={see ? 'text' : 'password'}
-          placeholder="password"
-          id="password"
-          name="password"
-          {...register('password')}
-        />
-        <button onClick={(ev) => handleClick(ev)}>see</button>
-        <button type="submit">Login</button>
-      </form>
-    </main>
+    <div className="login_container">
+      <main>
+        <div className="content">
+          <h2>Get Started</h2>
+          {error !== null && <h2>{error}</h2>}
+        </div>
+        <form onSubmit={handleSubmit(formSubmit)}>
+          <div className="user_container">
+            <input
+              className="input_user"
+              type="text"
+              placeholder={onFocus ? ' ' : 'username'}
+              onFocus={() => setOnFocus(true)}
+              onBlur={() => setOnFocus(false)}
+              id="username"
+              name="username"
+              {...register('username')}
+            />
+            <label htmlFor="custom-input" className="custom-placeholder">
+              username
+            </label>
+          </div>
+          <div className="password_container">
+            <input
+              className="input_user"
+              type={see ? 'text' : 'password'}
+              placeholder={onFocus ? ' ' : 'username'}
+              onFocus={() => setOnFocus(true)}
+              onBlur={() => setOnFocus(false)}
+              id="password"
+              name="password"
+              {...register('password')}
+            />
+            <label htmlFor="custom-input" className="custom-placeholder">
+              password
+            </label>
+          </div>
+          <div className="btn_container">
+            <button className="see_btn" onClick={(ev) => handleClick(ev)}>
+              {see ? (
+                <img
+                  src="https://res.cloudinary.com/dva9zee9r/image/upload/v1679514213/invisible_ljwcqc.png"
+                  alt="ojo cerrado"
+                />
+              ) : (
+                <img
+                  src="https://res.cloudinary.com/dva9zee9r/image/upload/v1679514203/visibilidad_c7kgso.png"
+                  alt="ojo"
+                />
+              )}
+            </button>
+            <button className="login_btn" type="submit">
+              Login
+            </button>
+          </div>
+          <div className="register_container">
+            <h4>Dont have an account yet?</h4>
+            <NavLink to="/register" className="register_nav">
+              {' '}
+              Register Here!{' '}
+            </NavLink>
+          </div>
+        </form>
+      </main>
+    </div>
   );
 };
 
