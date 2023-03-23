f60bedd2bcc55b4aff70c6099ab06042aea7412b
Adrián <123007421+Kirk810@users.noreply.github.com>
2023-03-23 16:25:27 +0100

    feature-modal

---
+++ TestBusters/src/ui/ModalTest.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Palette from '../styles/Palette';
+import Button from './Button';
+const EnemyCardStyled = styled.div`
+  & .hidden {
+    display: none !important;
+  }
+  & .modal-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 35%;
+    padding: 2rem;
+    gap: 2rem;
+    border-radius: 10px;
+    background-color: ${Palette.color_bg};
+    animation-name: vibrar;
+    animation-duration: 0.3s;
+    animation-timing-function: ease-out;
+  }
+  & .modal {
+    background-color: #cccccba7;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 999;
+  }
+  & .buttons {
+    display: flex;
+  }
+  @keyframes vibrar {
+    0% {
+      transform: scale(0.8);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+`;
+
+const ModalTest = ({ text }) => {
+  const [modal, setModal] = useState(false);
+  const navigate = useNavigate();
+
+  return (
+    <EnemyCardStyled>
+      <div>
+        <button
+          onClick={() => {
+            setModal(!modal);
+          }}
+        >
+          {text}
+        </button>
+      </div>
+      <div className={modal ? 'modal' : 'hidden'}>
+        <div className="modal-container">
+          <h3>Are you sure? You will lose all your progress.</h3>
+          <div className="buttons">
+            <Button
+              fixed_width={'100px'}
+              textAfter="No"
+              size="4"
+              margin={'10px'}
+              background={Palette.color_bg}
+              variant="border"
+              action={() => {
+                setModal(!modal);
+              }}
+            />
+            <Button
+              fixed_width={'100px'}
+              textAfter="Yes"
+              size="4"
+              margin={'10px'}
+              background={Palette.color_highlight_primary}
+              color={Palette.color_bg}
+              action={() => navigate('/tests')}
+            />
+          </div>
+        </div>
+      </div>
+    </EnemyCardStyled>
+  );
+};
+
+export default ModalTest;
