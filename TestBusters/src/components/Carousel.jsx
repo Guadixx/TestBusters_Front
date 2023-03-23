cc10efe52f6d34edba728a70ab5501ce51c2e8c5
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-03-23 16:18:38 +0100

    carrusel

--- TestBusters/src/components/Carousel.jsx
+++ TestBusters/src/components/Carousel.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import './Carousel.css';
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Carousel = () => {
   const [clicked, setClicked] = useState(false);
@@ -10,95 +10,156 @@ const Carousel = () => {
   const [firstClientXPosition, setFirstClientXPosition] = useState(0);
   const [lastPosition, setLastPosition] = useState(-window.innerWidth);
   const [animation, setAnimation] = useState(false);
+  const [loop, setLoop] = useState(false);
+  const [lastLoop, setLastLoop] = useState(false);
+  const [firstRender, setFirstRender] = useState(true);
   const div1 = useRef(null);
   const div2 = useRef(null);
   const div3 = useRef(null);
-  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  const list = [1, 2, 3, 4, 5, 6];
   const [index, setIndex] = useState(() => {
     return Math.floor(list.length / 2);
   });
+  const handleMouseDown = (ev) => {
+    setLoop(true);
+    setAnimation(false);
+    setFirstClientXPosition(ev.clientX);
+    setClicked(true);
+  };
+  const handleMouseUp = (ev) => {
+    const offset = Math.abs(ev.currentTarget.offsetLeft);
+    const _1 = div1.current.offsetLeft;
+    const _2 = div2.current.offsetLeft;
+    const _3 = div3.current.offsetLeft;
+    if (position != _2 * -1) {
+      setAnimation(true);
+    }
+    setPosition(() => {
+      return offset <= _2 / 2 ? _1 : offset >= _2 + _2 / 2 ? _3 * -1 : _2 * -1;
+    });
+    const lastActualized =
+      offset <= _2 / 2 ? _1 : offset >= _2 + _2 / 2 ? _3 * -1 : _2 * -1;
+    const indexActualized =
+      lastActualized == _1 ? index - 1 : lastActualized == _2 * -1 ? index : index + 1;
+    setLastPosition(lastActualized);
+    setClicked(false);
+    setTimeout(() => {
+      setIndex((prevIndex) => {
+        if (indexActualized == -1) {
+          return list.length - 1;
+        }
+        if (prevIndex == list.length - 1 && indexActualized == 0) {
+          return 0;
+        }
+        if (indexActualized == list.length) {
+          return 0;
+        } else {
+          return lastActualized == _1
+            ? prevIndex - 1
+            : lastActualized == _2 * -1
+            ? prevIndex
+            : prevIndex + 1;
+        }
+      });
+      setAnimation(false);
+      setPosition(_2 * -1);
+      setLastPosition(_2 * -1);
+    }, 700);
+  };
+  const handleMouseMove = (ev) => {
+    if (clicked) {
+      setPosition(() => {
+        return lastPosition + ev.clientX - firstClientXPosition;
+      });
+    }
+  };
+  /* ------------------------------------------ */
+  const moveLeft = (timeout) => {
+    const _2 = div2.current.offsetLeft;
+    const _3 = div3.current.offsetLeft;
+    setAnimation(true);
+    setPosition(_3 * -1);
+    const lastActualized = _3 * -1;
+    const indexActualized = index + 1;
+    setLastPosition(lastActualized);
+    setLastLoop(!lastLoop);
+    setTimeout(() => {
+      if (!clicked) {
+        setIndex((prevIndex) => {
+          if (indexActualized == -1) {
+            return list.length - 1;
+          }
+          if (prevIndex == list.length - 1 && indexActualized == 0) {
+            return 0;
+          }
+          if (indexActualized == list.length) {
+            return 0;
+          } else {
+            return prevIndex + 1;
+          }
+        });
+        setAnimation(false);
+        setPosition(_2 * -1);
+        setLastPosition(_2 * -1);
+      }
+    }, timeout);
+  };
+  useEffect(() => {
+    if (clicked || loop || firstRender) {
+      return;
+    }
+    setTimeout(() => {
+      moveLeft(700);
+    }, 2000);
+  }, [index, loop, firstRender]);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoop(false);
+      setFirstRender(false);
+    }, 1000);
+  }, []);
+  useEffect(() => {
+    if (loop) {
+      setAnimation(false);
+      const indexActualized = index - 1;
+      setIndex((prevIndex) => {
+        if (indexActualized == -1) {
+          return list.length - 1;
+        }
+        if (prevIndex == list.length - 1 && indexActualized == 0) {
+          return 0;
+        }
+        if (indexActualized == list.length) {
+          return 0;
+        } else {
+          return prevIndex - 1;
+        }
+      });
+    }
+  }, [lastLoop]);
   return (
     <div className="hola">
+      {loop && (
+        <button className="gibraltar" onClick={() => setLoop(false)}>
+          Loop
+        </button>
+      )}
       <div
-        style={{ left: `calc(${position}px)` }}
+        style={{ left: `${position}px` }}
         className={animation ? 'divcar animation' : 'divcar'}
         onMouseDown={(ev) => {
-          setAnimation(false);
-          setFirstClientXPosition(ev.clientX);
-          setClicked(true);
+          handleMouseDown(ev);
         }}
         onMouseUp={(ev) => {
-          setAnimation(true);
-          const offset = Math.abs(ev.currentTarget.offsetLeft);
-          const _1 = div1.current.offsetLeft;
-          const _2 = div2.current.offsetLeft;
-          const _3 = div3.current.offsetLeft;
-          setPosition(() => {
-            return offset <= _2 / 2 ? _1 : offset >= _2 + _2 / 2 ? _3 * -1 : _2 * -1;
-          });
-          const lastActualized =
-            offset <= _2 / 2 ? _1 : offset >= _2 + _2 / 2 ? _3 * -1 : _2 * -1;
-          const indexActualized =
-            lastActualized == _1
-              ? index - 1
-              : lastActualized == _2 * -1
-              ? index
-              : index + 1;
-          setLastPosition(lastActualized);
-          setClicked(false);
-          setTimeout(() => {
-            setIndex((prevIndex) => {
-              if (indexActualized == -1) {
-                return list.length - 1;
-              }
-              if (prevIndex == list.length - 1 && indexActualized == 0) {
-                return 0;
-              }
-              if (indexActualized == list.length) {
-                return 0;
-              } else {
-                return lastActualized == _1
-                  ? prevIndex - 1
-                  : lastActualized == _2 * -1
-                  ? prevIndex
-                  : prevIndex + 1;
-              }
-            });
-            setAnimation(false);
-            setPosition(_2 * -1);
-            setLastPosition(_2 * -1);
-          }, 500);
+          handleMouseUp(ev);
         }}
         onMouseLeave={(ev) => {
-          const offset = Math.abs(ev.currentTarget.offsetLeft);
-          const _1 = div1.current.offsetLeft;
-          const _2 = div2.current.offsetLeft;
-          const _3 = div3.current.offsetLeft;
-          setPosition(() => {
-            return offset <= _2 / 2 ? _1 : offset >= _2 + _2 / 2 ? _3 * -1 : _2 * -1;
-          });
-          const lastActualized =
-            offset <= _2 / 2 ? _1 : offset >= _2 + _2 / 2 ? _3 * -1 : _2 * -1;
-          setIndex((prevIndex) => {
-            return lastActualized == _1
-              ? prevIndex - 1
-              : lastActualized == _2 * -1
-              ? prevIndex
-              : prevIndex + 1;
-          });
-          setLastPosition(lastActualized);
-          setClicked(false);
-          setTimeout(() => {
-            setPosition(_2 * -1);
-            setLastPosition(_2 * -1);
-          }, 10);
+          if (clicked) {
+            handleMouseUp(ev);
+          }
         }}
         onMouseMove={(ev) => {
-          if (clicked) {
-            setPosition(() => {
-              return lastPosition + ev.clientX - firstClientXPosition;
-            });
-          }
+          handleMouseMove(ev);
         }}
       >
         <div className="aruba aruba1" ref={div1}>
