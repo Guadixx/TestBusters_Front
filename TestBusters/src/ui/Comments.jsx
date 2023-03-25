bd5fbdc59a60d6e75da11b0f6078eae7471b72a5
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-03-25 15:48:07 +0100

    tests 6

--- TestBusters/src/ui/Comments.jsx
+++ TestBusters/src/ui/Comments.jsx
@@ -1,45 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Comment = ({ avatar, name, date, comment }) => {
-  const CommentsStyled = styled.div`
+import Palette from '../styles/Palette';
+const CommentsStyled = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  & .date {
     display: flex;
-    flex-direction: row;
-    padding: 10px;
-    border-radius: 5px;
-    margin-bottom: 10px;
-    & .date {
-      display: flex;
-      gap: 20px;
-    }
-    & .avatar {
-      display: flex;
-    }
-  `;
-
-  const Avatar = styled.img`
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-  `;
-
-  const Name = styled.div`
-    text-align: left;
-    font-size: 20px;
-    font-weight: bold;
-    padding-left: 20px;
+    gap: 20px;
+  }
+  & .avatar {
     display: flex;
-  `;
+  }
+  & .button-close-comment {
+    height: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: '#0000000';
+    color: ${Palette.color_highlight_secondary};
+    font-weight: 900;
+  }
+`;
 
-  const Date = styled.div`
-    font-size: 20px;
-  `;
-  const Comment = styled.div`
-    text-align: left;
-    width: 500px;
-    padding-left: 20px;
-  `;
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
 
+const Name = styled.div`
+  text-align: left;
+  font-size: 20px;
+  font-weight: bold;
+  padding-left: 20px;
+  display: flex;
+`;
+
+const Date = styled.div`
+  font-size: 20px;
+`;
+const Content = styled.div`
+  text-align: left;
+  width: 500px;
+  padding-left: 20px;
+`;
+const Comment = ({ avatar, name, date, content, action, idUserOnComment, idUser }) => {
   return (
     <CommentsStyled>
       <div className="avatar">
@@ -49,8 +58,15 @@ const Comment = ({ avatar, name, date, comment }) => {
             <Name>{name}</Name>
             <Date>{date}</Date>
           </div>
-          <Comment>{comment}</Comment>
+          <Content>{content}</Content>
         </div>
+        {idUserOnComment == idUser ? (
+          <button onClick={action} className="button-close-comment">
+            X
+          </button>
+        ) : (
+          <div></div>
+        )}
       </div>
     </CommentsStyled>
   );
