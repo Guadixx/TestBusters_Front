fe00404bd430cc185861c4e1637f6a62915fbe33
Guadalupe <guada@AirdeGuadalupe.home>
2023-03-27 15:23:22 +0200

    trying to push

--- TestBusters/src/pages/NotFound.jsx
+++ TestBusters/src/pages/NotFound.jsx
@@ -1,7 +1,17 @@
 import './NotFound.css';
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="notFound_container">
+      <div className="notfound_container">
+        <h3>404 Not Found</h3>
+      </div>
+      <div className="message_container">
+        <h4> Sorry!! looks like the page you are looking for doesnt exist. </h4>
+        <h4>Try again with a different one!</h4>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
