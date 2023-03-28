8cff44ac7a5f54517622ac30663b2c77142f934e
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-03-28 20:13:43 +0200

    home-random-daytest

--- TestBusters/src/components/RandomTest.jsx
+++ TestBusters/src/components/RandomTest.jsx
@@ -4,22 +4,35 @@ import TestCard from '../components/TestCard';
 import { API } from '../services/API';
 
 const RandomTest = () => {
-  const [test, setTest] = useState([]);
-
+  const [test, setTest] = useState(undefined);
+  const [random, SetRandom] = useState(true);
+  const [loaded, setLoaded] = useState(true);
   useEffect(() => {
-    API.get('featuredTests').then((response) => setTest(response.data));
-  }, []);
-
-  const randomIndex = Math.floor(Math.random() * test.length);
-  const randomTest = test[randomIndex];
+    setLoaded(true);
+    const type = random ? 'featuredTests' : 'generictests';
+    API.get(`/${type}/random`)
+      .then((res) => {
+        if (res.status == 200) {
+          setTest(res.data);
+          setLoaded(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [random]);
 
   return (
     <div>
-      <>
-        {randomTest.map((test) => (
-          <TestCard test={test} key={test.id} />
-        ))}
-      </>
+      {test != undefined ? <TestCard test={test} /> : <h3>Loading...</h3>}
+      <button
+        disabled={loaded}
+        onClick={() => {
+          SetRandom(!random);
+        }}
+      >
+        Get Random
+      </button>
     </div>
   );
 };
