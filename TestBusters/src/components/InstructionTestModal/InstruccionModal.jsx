716cb5ccf671aa1309743dcc8e2783718f7b1d46
Guadalupe <guada@AirdeGuadalupe.home>
2023-03-29 18:42:58 +0200

    instruccion changes

--- TestBusters/src/components/InstructionTestModal/InstruccionModal.jsx
+++ TestBusters/src/components/InstructionTestModal/InstruccionModal.jsx
@@ -4,64 +4,6 @@ const CreateTestModal = ({ setShowInstruccionModal }) => {
   return (
     <div className="createtest-modal">
       <div className="createtest-modal-content">
-        <h1>Instructions</h1>
-        <div className="textintruccion_container">
-          <h4>On the left side of the page you have 4 filters you set on your likings</h4>
-          <div className="list_container">
-            <ul>
-              <li>
-                <p>1st: Time. You set how long the test will last </p>
-              </li>
-              <li>
-                <p>
-                  2nd: The order. Will your questions have an order or do you want them to
-                  show up randomly?
-                </p>
-              </li>
-              <li>
-                <p>
-                  3rd: Comments. You can choose if you want the comment section to be
-                  available in your test or not
-                </p>
-              </li>
-              <li>
-                <p>4th: Topic. What is your test about? Enter your chosen topic.</p>
-              </li>
-            </ul>
-          </div>
-          <div className="middleh4title_container">
-            <h4> On the right side of the page you have two sections</h4>
-          </div>
-          <div className="middleh4_container">
-            <h4>
-              First two options is to upload a Thumbnail and a Banner, this will show in
-              the main page as a thumbnail for your test
-            </h4>
-            <h4>
-              Then you choose a Title and description for your test, so the audience knows
-              what is about!
-            </h4>
-          </div>
-          <div className="middleh4title_container">
-            <h4>On the bottom have of the page you have the last part</h4>
-          </div>
-          <div className="bottomh4_container">
-            <h4>
-              You choose either if your question would be a written question (text) or an
-              image{' '}
-            </h4>
-            <h4>On the right side you can chose to upload an extra picture</h4>
-            <h4>
-              The + sign is to upload another answer option, you can add as many as you
-              like
-            </h4>
-            <h4> The trashcan is to delete the whole test, be careful!</h4>
-            <h4>
-              + Add question button, is to add another question. Again add as many as you
-              like!
-            </h4>
-          </div>
-        </div>
         <div className="btnclose_container">
           <button className="btn_close" onClick={() => setShowInstruccionModal(false)}>
             <img
@@ -69,6 +11,67 @@ const CreateTestModal = ({ setShowInstruccionModal }) => {
               alt="close icon"
             />
           </button>
+        </div>
+        <h1>Instructions</h1>
+        <div className="textintruccion_container">
+          <div className="list_container">
+            <ul>
+              <li>
+                <h4>Time</h4>
+                <p>Time. You set how long the test will last </p>
+              </li>
+              <li>
+                <h4>Order</h4>
+                <p>
+                  The order. Will your questions have an order or do you want them to show
+                  up randomly?
+                </p>
+              </li>
+              <li>
+                <h4>Comment</h4>
+                <p>
+                  Comments. You can choose if you want the comment section to be available
+                  in your test or not
+                </p>
+              </li>
+              <li>
+                <h4>Topic</h4>
+                <p>Topic. What is your test about? Enter your chosen topic.</p>
+              </li>
+            </ul>
+          </div>
+          <div className="middlelist_container">
+            <ul>
+              <li>
+                <h4>Thumbnail and a Banner </h4>
+                <p>This will show in the main page as a thumbnail for your test</p>
+              </li>
+              <li>
+                <h4>Title and description</h4>
+                <p>
+                  Title and description for your test, so the audience knows what is
+                  about!
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className="middlelist_container">
+            <h4>Custom Section</h4>
+            <p>
+              You choose either if your question would be a written question (text) or an
+              image{' '}
+            </p>
+            <p>On the right side you can chose to upload an extra picture</p>
+            <p>
+              The + sign is to upload another answer option, you can add as many as you
+              like
+            </p>
+            <p> The trashcan is to delete the whole test, be careful!</p>
+            <p>
+              + Add question button, is to add another question. Again add as many as you
+              like!
+            </p>
+          </div>
         </div>
       </div>
     </div>
